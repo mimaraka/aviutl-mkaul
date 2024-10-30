b901b9cb6d501df923e7ea23b75f3a66d037fcfd
mimaraka <nj.ot.niconico@gmail.com>
2024-10-30 17:53:18 +0900

    Update variant.hpp

--- mkaul/variant.hpp
+++ mkaul/variant.hpp
@@ -156,7 +156,7 @@ namespace mkaul {
 			}
 
 			template<typename T>
-			HRESULT set(T val) {
+			HRESULT set(T&& val) {
 				if constexpr (detail::is_vector<T>::value) {
 					using value_type = typename T::value_type;
 					auto vt = get_vt<value_type>();
@@ -167,7 +167,7 @@ namespace mkaul {
 						Variant tmp;
 						auto hr = tmp.set(val[i]);
 						if (SUCCEEDED(hr)) {
-							hr = ::SafeArrayPutElement(p_safe_array, (LONG*)&i, to_element_ptr(tmp));
+							hr = ::SafeArrayPutElement(p_safe_array, (LONG*)&i, to_element_ptr(tmp.variant_));
 						}
 						if (FAILED(hr)) {
 							::SafeArrayDestroy(p_safe_array);
